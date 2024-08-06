73c3659b936f014eb2748ac2f5f7b51e5cabec59
TECNIMUX <76859976+TECNIMUX-DEV@users.noreply.github.com>
2024-08-06 01:39:14 -0500

    Adjust all sections

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 // Add type of language
-let languageState = document.getElementById('language-state')
-function changeTextES () {
-    languageState.innerHTML = 'Español 🇪🇸';
-}
-function changeTextEN () {
-    languageState.innerHTML = 'English 🇺🇸';
-}
+// let languageState = document.getElementById('language-state')
+// function changeTextES () {
+//     languageState.innerHTML = 'Español 🇪🇸';
+// }
+// function changeTextEN () {
+//     languageState.innerHTML = 'English 🇺🇸';
+// }
 
-let imgLanguage = document.getElementById('img-languages')
-let optionsLanguages = document.getElementById('options-languages')
-imgLanguage.addEventListener('click', showOptions)
-function showOptions() {
-    imgLanguage.style.display = 'none'
-    optionsLanguages.style.display = 'inline-flex'
-}
+// let imgLanguage = document.getElementById('img-languages')
+// let optionsLanguages = document.getElementById('options-languages')
+// imgLanguage.addEventListener('click', showOptions)
+// function showOptions() {
+//     imgLanguage.style.display = 'none'
+//     optionsLanguages.style.display = 'inline-flex'
+// }
 
-let es = document.getElementById('es')
-let en = document.getElementById('en')
-es.addEventListener('click', (changeTextES))
-en.addEventListener('click', (changeTextEN))
+// let es = document.getElementById('es')
+// let en = document.getElementById('en')
+// es.addEventListener('click', (changeTextES))
+// en.addEventListener('click', (changeTextEN))
 
 
 
 // Hidden nav language
-let hL = function hiddenLanguage () {
-    if (window.scrollY > 1) {
-            language.style.opacity = '0';
-            language.classList.remove('show-a')
-            language.classList.add('show-r')
-        } else {
-            language.style.opacity = '1';
-            language.classList.remove('show-r')
-            language.classList.add('show-a')
-        }
-    }
+// let hL = function hiddenLanguage () {
+//     if (window.scrollY > 1) {
+//             language.style.opacity = '0';
+//             language.classList.remove('show-a')
+//             language.classList.add('show-r')
+//         } else {
+//             language.style.opacity = '1';
+//             language.classList.remove('show-r')
+//             language.classList.add('show-a')
+//         }
+//     }
 
-let language = document.getElementById('language')
-window.addEventListener('scroll', (hL))
-
-
-let contentSkill = document.getElementById('content-skill') 
-    let contentSkillHTML = document.getElementById('content-skill-HTML')
-
-function showTap() {
-    contentSkill.style.display = 'block';
-    }
+// let language = document.getElementById('language')
+// window.addEventListener('scroll', (hL))
 
 
-function showTapHTML() {
-    contentSkillHTML.style.display = 'block';
-    }
+// let contentSkill = document.getElementById('content-skill') 
+//     let contentSkillHTML = document.getElementById('content-skill-HTML')
+
+// function showTap() {
+//     contentSkill.style.display = 'block';
+//     }
+
+
+// function showTapHTML() {
+//     contentSkillHTML.style.display = 'block';
+//     }
 
 
 // Click on skills
